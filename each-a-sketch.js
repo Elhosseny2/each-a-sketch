921f39fa0fa45ea5339a4f92c08cb006b8bccf1f
Elhosseny2 <moelhosiny8@gmail.com>
2024-09-28 10:21:55 +0300

    COMMIT always and update
    did not forget this time but don't want git hub be
    a witness to the shit i code

--- each-a-sketch.js
+++ each-a-sketch.js
@@ -1,3 +1,34 @@
 let container = document.querySelector("#container");
 
-container.appendChild();
+function makeRow(row, ) {
+  for (i = 0; i < (row * 16); i++) {
+    let row = document.createElement("div");
+    // let culms = document.createElement("div");
+    container.appendChild(row).className = "row"
+    // container.appendChild(culms).className = "culms"
+    row.textContent="row"
+    // culms.textContent="culms"
+  }
+}
+makeRow(2);
+
+// function makeCulms(culms ) {
+//     for (i = 0; i < (culms * 16); i++) {
+//       let culms = document.createElement("div");
+//     //   let culms = document.createElement("div");
+//       container.appendChild(culms).className = "culms"
+//       culms.textContent="culms"
+//     }
+//   }
+//   makeCulms(1);
+
+//   let fMakeRow = makeRow()
+//   let fMakeCulms = makeCulms()
+
+//   function makeGrid (fMakeRow, fMakeCulms){
+//     for (i = 0; i < (fMakeRow * fMakeCulms); i++) {
+//         fMakeRow.textContent = "grid"
+//         fMakeCulms.textContent = "grid"
+//   }
+// }
+// makeGrid(fMakeRow,fMakeCulms)
